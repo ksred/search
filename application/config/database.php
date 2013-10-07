bebab7ca0e9ec55107cc0ff3d0862cf8220d6a39
Kyle Redelinghuys <kyle@ksred.me>
2013-10-07 13:39:27 +0200

    Database created and details updated

--- application/config/database.php
+++ application/config/database.php
@@ -42,9 +42,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['username'] = 'search_u';
+$db['default']['password'] = '35^8%7WP*X)vU63';
+$db['default']['database'] = 'search';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
